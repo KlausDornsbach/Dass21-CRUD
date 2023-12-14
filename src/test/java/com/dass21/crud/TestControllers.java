a957b23fabefa1bbdaa4cb10432227a0f7fc6ff4
klausdornsbach <klausdornsbach@gmail.com>
2023-12-14 02:22:44 -0300

    fix: better and more tests

--- src/test/java/com/dass21/crud/TestControllers.java
+++ src/test/java/com/dass21/crud/TestControllers.java
@@ -1,10 +1,12 @@
 package com.dass21.crud;
 
+import com.dass21.crud.controller.FullController;
 import com.dass21.crud.controller.ParticipanteController;
 import com.dass21.crud.controller.RespostaController;
 import com.dass21.crud.domain.Condicao;
 import com.dass21.crud.domain.Participante;
 import com.dass21.crud.domain.Resposta;
+import com.dass21.crud.dto.FormDTO;
 import com.dass21.crud.dto.ParticipanteDTO;
 import com.dass21.crud.dto.ParticipanteRespostaDTO;
 import com.dass21.crud.dto.RespostaDTO;
@@ -29,6 +31,8 @@ public class TestControllers {
     ParticipanteController participanteController;
     @Autowired
     RespostaController respostaController;
+    @Autowired
+    FullController fullController;
     @Autowired
     RespostaRepository respostaRepository;
     @Autowired
@@ -60,7 +64,7 @@ public class TestControllers {
         Resposta savedResposta = respostaRepository.save(new Resposta(savedParticipante, respostaDTO));
 
         // verification
-        ResponseEntity<List<ParticipanteRespostaDTO>> response = participanteController.handleGetAllParticipantesRespostas();
+        ResponseEntity<List<ParticipanteRespostaDTO>> response = fullController.handleGetAllParticipantesRespostas();
 
         assertEquals(response.getStatusCode(), HttpStatus.ACCEPTED);
 
@@ -73,10 +77,10 @@ public class TestControllers {
 
         // only test some random properties, can't test the whole thing because localDateTime floating point precision
         // gets in the way
-        assertEquals(returned.participante_id(), participanteRespostaDTO.participante_id());
+        assertEquals(returned.participanteId(), participanteRespostaDTO.participanteId());
         assertEquals(returned.idade(), participanteRespostaDTO.idade());
-        assertEquals(returned.pontuacao_total_ansiedade(), participanteRespostaDTO.pontuacao_total_ansiedade());
-        assertEquals(returned.escala_depressao(), participanteRespostaDTO.escala_depressao());
+        assertEquals(returned.pontuacaoTotalAnsiedade(), participanteRespostaDTO.pontuacaoTotalAnsiedade());
+        assertEquals(returned.escalaDepressao(), participanteRespostaDTO.escalaDepressao());
     }
 
     @Test
@@ -90,7 +94,7 @@ public class TestControllers {
         respostaRepository.save(new Resposta(savedParticipante, respostaDTO));
 
         // verification
-        ResponseEntity<List<ParticipanteRespostaDTO>> response = participanteController.handleGetAllParticipantesRespostas();
+        ResponseEntity<List<ParticipanteRespostaDTO>> response = fullController.handleGetAllParticipantesRespostas();
 
         assertEquals(response.getStatusCode(), HttpStatus.ACCEPTED);
     }
@@ -130,8 +134,7 @@ public class TestControllers {
 
         RespostaDTO newRespostaDTO = new RespostaDTO(savedParticipante.getId(), 10, 10, 10);
 
-        ResponseEntity<String> response = respostaController.handlePutResposta(savedResposta.getId(),
-                newRespostaDTO);
+        ResponseEntity<String> response = respostaController.handlePutResposta(newRespostaDTO);
 
         Resposta newlySaved = respostaRepository.findRespostaById(savedResposta.getId()).get();
 
@@ -161,7 +164,7 @@ public class TestControllers {
         assertEquals(response.getStatusCode(), HttpStatus.ACCEPTED);
         ParticipanteRespostaDTO returned = Objects.requireNonNull(response.getBody());
         assertEquals(returned.idade(), savedParticipante.getIdade());
-        assertEquals(returned.pontuacao_total_depressao(), savedResposta.getPontuacaoTotalDepressao());
+        assertEquals(returned.pontuacaoTotalDepressao(), savedResposta.getPontuacaoTotalDepressao());
     }
 
     @Test
@@ -170,5 +173,66 @@ public class TestControllers {
 
         // verification
         assertEquals(response.getStatusCode(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    void addParticipanteWithForm() {
+        long countBeforeParticipantes = participanteRepository.count();
+        long countBeforeRespostas = respostaRepository.count();
+        FormDTO formDTO = new FormDTO("test_name", 1, "test_gender", 0, 0, 0);
+        ResponseEntity<String> response = fullController.handlePostForm(formDTO);
+
+        assertEquals(response.getStatusCode(), HttpStatus.ACCEPTED);
+        assertEquals(countBeforeParticipantes + 1, participanteRepository.count());
+        assertEquals(countBeforeRespostas + 1, respostaRepository.count());
+    }
+
+    @Test
+    void addMultipleRespostasReturnsLastOne() {
+        ParticipanteDTO participanteDTO = new ParticipanteDTO("user", 20, "Masculino");
+        Participante savedParticipante = participanteRepository.save(new Participante(participanteDTO));
+
+        RespostaDTO respostaDTO = new RespostaDTO(savedParticipante.getId(), 0, 0, 0);
+        respostaController.handleAddResposta(respostaDTO);
+
+        RespostaDTO respostaDTO2 = new RespostaDTO(savedParticipante.getId(), 10, 10, 10);
+        respostaController.handleAddResposta(respostaDTO2);
+
+        ResponseEntity<ParticipanteRespostaDTO> response = participanteController.handleGetSingleParticipante(savedParticipante.getId());
+        assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
+        assertEquals(10, response.getBody().pontuacaoTotalDepressao());
+        assertEquals(10, response.getBody().pontuacaoTotalAnsiedade());
+        assertEquals(10, response.getBody().pontuacaoTotalEstresse());
+    }
+
+
+    @Test
+    void updateParticipanteWithForm() {
+        FormDTO formDTO = new FormDTO("test_name", 1, "test_gender", 0, 0, 0);
+        fullController.handlePostForm(formDTO);
+
+        FormDTO formDTO1 = new FormDTO("other_gender");
+        Long id = participanteRepository.findAll().get(0).getId();
+        ResponseEntity<Object> response = fullController.handlePutForm(id, formDTO1);
+
+        assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
+        List<Object> returned = (List<Object>) response.getBody();
+        Participante p = (Participante) returned.get(0);
+        assertEquals("other_gender", p.getGenero());
+    }
+
+    @Test
+    void updateRespostaWithForm() {
+        FormDTO formDTO = new FormDTO("test_name", 1, "test_gender", 0, 0, 0);
+        fullController.handlePostForm(formDTO);
+
+        FormDTO formDTO1 = new FormDTO(10);
+        Long id = participanteRepository.findAll().get(0).getId();
+        ResponseEntity<Object> response = fullController.handlePutForm(id, formDTO1);
+
+        assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
+        List<Object> returned = (List<Object>) response.getBody();
+        Resposta r = (Resposta) returned.get(1);
+        assertEquals(10, r.getPontuacaoTotalEstresse());
     }
 }
